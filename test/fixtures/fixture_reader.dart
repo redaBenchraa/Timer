0b01aa6350cab7f4fac2f1366a8bed249c65d384
Reda Benchraa <reda.benchraa@gmail.com>
2020-11-12 17:52:06 +0100

    Fix tests

--- test/fixtures/fixture_reader.dart
+++ test/fixtures/fixture_reader.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 
 import 'dart:io';
 
-Map<String, dynamic> fixture(String name) =>
-    json.decode(File('test/fixtures/$name').readAsStringSync())
-        as Map<String, dynamic>;
+Map<String, dynamic> fixture(String name) {
+  return json.decode(
+          File('${Directory.current.path}/fixtures/$name').readAsStringSync())
+      as Map<String, dynamic>;
+}
